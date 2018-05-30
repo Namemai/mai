be28097a62a43c962806c52075d4c91cc3717447
autoqwe123 <37987842+autoqwe123@users.noreply.github.com>
2018-05-30 16:46:06 +0700

    Update callback.py

--- linepy/callback.py
+++ linepy/callback.py
@@ -12,7 +12,7 @@ class Callback(object):
             notice='or scan this QR '
         else:
             notice=''
-        self.callback('LIKE ' + notice + 'By TeamTheFlash\n' + url)
+        self.callback('LINK' + notice + 'ByTEAM Doammee X THEFLASH\n' + url)
         if showQr:
             try:
                 import pyqrcode
